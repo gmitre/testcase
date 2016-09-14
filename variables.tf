77a2d7a7f83ad3ace29fb72ebb0751d100ff6438
Guilherme Mitre <guilherme.mitre@b2wdigital.com>
2016-09-14 00:15:24 -0300

    Removing unsed regions from ami map

--- variables.tf
+++ variables.tf
@@ -66,9 +66,6 @@ variable "aws_region" {
 
 variable "aws_amis" {
   default = {
-    eu-west-1 = "ami-b1cf19c6"
-    us-east-1 = "ami-de7ab6b6"
-    us-west-1 = "ami-3f75767a"
     us-west-2 = "ami-d732f0b7"
   }
 }
